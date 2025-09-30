49b32f2415747acea7e3cd77bae6510785bd7c69
COKEiiii <827245412@qq.com>
2025-09-30 15:21:50 +0800

    Initial commit: Set up Spring Boot project with base dependencies (Web, JPA, Thymeleaf, MySQL)

---
+++ src/main/java/sg/edu/nus/team6_springbootca/Team6SpringBootCaApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.team6_springbootca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6SpringBootCaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team6SpringBootCaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/edu/nus/team6_springbootca/Team6SpringBootCaApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.team6_springbootca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team6SpringBootCaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
